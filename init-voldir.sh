ec5bbbf8fcb56688429f6797dc3c1d55c6a85df9
D4void <d4void@m4he.fr>
2024-04-27 23:27:23 +0200

    update

--- init-voldir.sh
+++ init-voldir.sh
@@ -7,5 +7,7 @@ mkdir -p ${TRAEFIK_VOL}/etc/traefik
 mkdir -p ${TRAEFIK_VOL}/log
 mkdir -p ${LETSENCRYPT_VOL}
 
+chmod 750 ${TRAEFIK_VOL}/etc/traefik
 cp ./traefik-config/traefik.toml.example ${TRAEFIK_VOL}/etc/traefik/traefik.toml
+chmod 640 ${TRAEFIK_VOL}/etc/traefik/traefik.toml
 cp -R ./traefik-config/dynamic/ ${TRAEFIK_VOL}/etc/traefik/
