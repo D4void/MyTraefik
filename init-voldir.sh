f31eb5e70e21345a2f44c68731f70db90d585b37
D4void <d4void@m4he.fr>
2024-05-07 18:23:04 +0200

    update

--- init-voldir.sh
+++ init-voldir.sh
@@ -3,15 +3,17 @@
 
 source .env
 
-mkdir -p ${TRAEFIK_VOL}/etc/traefik
-mkdir -p ${TRAEFIK_VOL}/log
-mkdir -p ${LETSENCRYPT_VOL}
+if [[ -d ${TRAEFIK_VOL} && -d ${LETSENCRYPT_VOL} ]]; then
+    mkdir -p ${TRAEFIK_VOL}/etc/traefik
+    mkdir -p ${TRAEFIK_VOL}/log
+    mkdir -p ${LETSENCRYPT_VOL}
 
-chmod 750 ${TRAEFIK_VOL}/etc/traefik
-cp ./traefik-config/traefik.toml.example ${TRAEFIK_VOL}/etc/traefik/traefik.toml
-chmod 640 ${TRAEFIK_VOL}/etc/traefik/traefik.toml
-cp -R ./traefik-config/dynamic/ ${TRAEFIK_VOL}/etc/traefik/
+    chmod 750 ${TRAEFIK_VOL}/etc/traefik
+    cp ./traefik-config/traefik.toml.example ${TRAEFIK_VOL}/etc/traefik/traefik.toml
+    chmod 640 ${TRAEFIK_VOL}/etc/traefik/traefik.toml
+    cp -R ./traefik-config/dynamic/ ${TRAEFIK_VOL}/etc/traefik/
 
-echo "Complete email address required by Letsencrypt in ${TRAEFIK_VOL}/etc/traefik/traefik.toml"
+    echo "Complete email address required by Letsencrypt in ${TRAEFIK_VOL}/etc/traefik/traefik.toml"
+fi
 
 echo "End."
